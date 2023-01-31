fd5763e0674e387b909c41884cd68ad955c46b50
Edward Sandor <public+git@sandorlaboratories.com>
2023-01-30 23:44:21 -0700

    Version 0.0.2-dev

--- xkcd_archiver.py
+++ xkcd_archiver.py
@@ -9,7 +9,7 @@ import datetime
 from   urllib.parse import urlparse
 import urllib.request, json 
 
-version_str="0.0.1"
+version_str="0.0.2-dev"
 publish_year_str="January 2023"
 author_str="Edward Sandor"
 
